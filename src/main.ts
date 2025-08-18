2b7af05b01a132e82537bf68a9b0495089caee8b
Your Name <you@example.com>
2025-08-18 15:15:46 +0700

    fix add documents swagger

--- src/main.ts
+++ src/main.ts
@@ -6,18 +6,18 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // Validation global
   app.useGlobalPipes(new ValidationPipe());
 
-  // Swagger setup
-  const config = new DocumentBuilder()
-    .setTitle('RBAC API')
-    .setDescription('API documentation for RBAC example')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('documents', app, document);
+  if (process.env.NODE_ENV !== 'production') {
+    const config = new DocumentBuilder()
+      .setTitle('RBAC API')
+      .setDescription('API documentation for RBAC example')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('documents', app, document);
+  }
 
   await app.listen(process.env.PORT ?? 8000);
 }
